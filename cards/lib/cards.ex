622e83a12038ff8be56ac84164d1b93211889c89
Zahra Khan <zahra.khan1@icloud.com>
2023-04-03 13:58:38 -0400

    create_hand with pipe operator

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,13 +1,11 @@
 defmodule Cards do
-
   def create_deck do
-   values = ["Ace", "Two", "Three", "Four", "Five"]
-   suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
@@ -28,11 +26,15 @@ defmodule Cards do
   end
 
   def load(filename) do
-
     case File.read(filename) do
-      {:ok, binary} -> :erlang.binary_to_term binary
+      {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "That file does not exist"
     end
   end
 
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+  end
 end
